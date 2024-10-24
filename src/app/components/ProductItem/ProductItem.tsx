83655f503b0179b948612e84df6196a153b9ed97
Pham Duy <duyp7454@gmail.com>
2024-10-24 11:27:40 +0700

    day code

--- src/app/components/ProductItem/ProductItem.tsx
+++ src/app/components/ProductItem/ProductItem.tsx
@@ -12,17 +12,9 @@ function ProductItem(props) {
    className="border p-1 relative group transition-all duration-500 w-[270px] h-[360px] hover:h-[410px]"
 
  >
-   {props.product.image ? (
-     <img
-       src={props.product.image}
-       alt={props.product.name}
-       className="w-full h-auto mb-2"
-     />
-   ) : (
-     <div className="w-full h-[273px] bg-gray-200 flex items-center justify-center">
-       <span className="text-gray-500 text-sm">No Image Available</span>
-     </div>
-   )}
+       <div className="bg-gray-200 h-52 w-full flex items-center justify-center">
+        <div className="text-gray-600">270x368</div>
+      </div>
 
    <div className="absolute top-1 left-2 flex space-x-2">
      <span className="bg-blue-500 text-white text-xs px-2 py-1 rounded">
